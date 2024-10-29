9a6d2ea28a12b46a5fc2d97ab543ee618ff0455e
Arthur Lino <arthurkglsilva@gmail.com>
2024-10-29 10:55:35 -0300

    added script to group .cdr files by yyyy-mm and move them to separate folders

---
+++ group_files_in_new_directory.ps1
@@ -0,0 +1,15 @@
+$filesToGroup = (Get-ChildItem (Get-Item .).FullName -Filter "*.CDR" | #change extension before use 
+Group {$_.LastWriteTime.ToString("yyyy-MM")})
+
+foreach ($f in $filesToGroup) {
+    mkdir $f.Name
+}
+
+foreach ($f in $filesToGroup) {
+    foreach ($subf in $f.group) {
+        $from = $subf.Name
+        $to = Join-Path (Get-Item .) $f.Name
+        Copy-Item $from $to
+        Remove-Item $from
+    }
+}
